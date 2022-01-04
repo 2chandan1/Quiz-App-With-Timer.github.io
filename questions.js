5ce09b6e870e8cdb660fc1814a06aaf0dd0379e4
Chandan Tiwari <Tiwarichandan936@gmail.com>
2022-01-04 18:53:12 +0530

    Quiz-App-With-timer

---
+++ questions.js
@@ -0,0 +1,59 @@
+// TODO:CREATING AN ARRAY AND PASSING THE NUMBER, QUESTIONS,OPTIONS,AND ANSWERS
+
+let questions = [
+  {
+    numb: 1,
+    question: "what does HTML  stand for ?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Markup Language",
+      "Holistick Technical Method Library",
+      "Hyper Tax Makes Line",
+      "None of the above",
+    ],
+  },
+  {
+    numb: 2,
+    question: "Generally, there are ?",
+    answer: "6 headers in most HTML page",
+    options: [
+      "4 headers in most HTML page",
+      "5 headers in most HTML page",
+      "6 headers in most HTML page",
+      "7 headers in most HTML page",
+    ],
+  },
+  {
+    numb: 3,
+    question: "To create HTML page, you need ?",
+    answer: "Both [A] and [B]",
+    options: [
+      " Web browser",
+      "  text editor",
+      "Both [A] and [B]",
+      " None of the above",
+    ],
+  },
+  {
+    numb: 4,
+    question: "The correct sequence of HTML tags for starting a webpage is ?",
+    answer: " HTML, Head, Title, Body",
+    options: [
+      " Head, Title, HTML, body",
+      " HTML, Body, Title, Head",
+      " HTML, Head, Title, Body",
+      " HTML, Head,Body,Title",
+    ],
+  },
+  {
+    numb: 5,
+    question: "Which program do you need to write HTML?",
+    answer: "Any text editor",
+    options: [
+      " A graphics program",
+      "Any text editor",
+      " HTML -development suite 4",
+      " All of the above",
+    ],
+  },
+];
